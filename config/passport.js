a05b0b8fa1e95c70bcbbcdddf55fcb8e8c690fdb
yatharth <yatharthverma070@gmail.com>
2020-06-21 18:13:45 +0530

    :art: - arrow notation

--- config/passport.js
+++ config/passport.js
@@ -13,11 +13,11 @@ module.exports = (passport) => {
       }
     ));
 
-    passport.serializeUser(function(user, done) {
+    passport.serializeUser((user, done) => {
         done(null, user.id);
     });
       
-    passport.deserializeUser(function(id, done) {
+    passport.deserializeUser((id, done) => {
         User.findById(id, function(err, user) {
           done(err, user);
         });
