e37a5961e6ab362628c7d1406cc9256b4257e4e2
yatharth <yatharthverma070@gmail.com>
2020-06-21 23:16:12 +0530

    :bookmark: - Add stories to dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
+const Story = require('../models/Story');
 
 /** 
  * GET /
@@ -19,10 +20,20 @@ router.get('/', ensureGuest, (req,res) => {
  * GET /dashboard
  * Function: Dashboard
  */
-router.get('/dashboard', ensureAuth, (req,res) => {
-    res.render('dashboard', {
-        name: req.user.firstName
-    })
+router.get('/dashboard', ensureAuth, async (req,res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id}).lean()
+        
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+
+    } catch(e) {
+        res.render('errors/500')
+    }
+    
+    
 })
 
 
