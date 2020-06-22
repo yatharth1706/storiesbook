a3f28e8beb3f4b107198caeabee53e518c3288c7
yatharth <yatharthverma070@gmail.com>
2020-06-23 00:08:29 +0530

    :lipstick: - Add more middlewares

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connectDb = require('./config/db');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override'); 
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -28,11 +29,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 //Handlebar helpers
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
+
 
 
 // Settings for handlebars
-app.engine('.hbs', exphbs({ helpers: {formatDate,} ,defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({ helpers: {formatDate,stripTags, truncate, editIcon, select} ,defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // Session Middleware
@@ -46,6 +49,24 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global variable
+app.use(function(req,res,next){
+    res.locals.user = req.user || null
+    next(); 
+})
+
+
+// Method Override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
+
 
 // Static files access configuration
 app.use(express.static(path.join(__dirname, 'public')))
